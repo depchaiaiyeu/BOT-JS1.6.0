355a3c147ad84add9f8a5e15fc8632aeeb20e01a
depchaiaiyeu <kiennone@gmail.com>
2025-10-04 21:23:25 +0700

    Update register-fonts.js

--- src/utils/canvas/register-fonts.js
+++ src/utils/canvas/register-fonts.js
@@ -1,6 +1,10 @@
 import { registerFont } from "canvas";
 import path from "path";
+import { fileURLToPath } from "url";
 
-registerFont(path.resolve("../../assets/fonts/SVN-Transformer.ttf"), { family: "Transformer" });
-registerFont(path.resolve("../../assets/fonts/BeVietnamPro-Bold.ttf"), { family: "BeVietnamPro" });
-registerFont(path.resolve("../../assets/fonts/NotoEmoji-Bold.ttf"), { family: "NotoEmojiBold" });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+registerFont(path.join(__dirname, "../../assets/fonts/SVN-Transformer.ttf"), { family: "Transformer" });
+registerFont(path.join(__dirname, "../../assets/fonts/BeVietnamPro-Bold.ttf"), { family: "BeVietnamPro" });
+registerFont(path.join(__dirname, "../../assets/fonts/NotoEmoji-Bold.ttf"), { family: "NotoEmojiBold" });
