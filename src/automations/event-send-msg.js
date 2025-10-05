c6d1f489f4583e8c813c4e43814f6ab52a9b831d
depchaiaiyeu <kiennone@gmail.com>
2025-10-05 13:29:46 +0700

    Update event-send-msg.js

--- src/automations/event-send-msg.js
+++ src/automations/event-send-msg.js
@@ -169,7 +169,7 @@ export async function messagesUser(api, message) {
       }
 
       let handleChat = true;
-      handleChat = handleChat && !(await superCheckBox(api, message, isSelf, botIsAdminBox, isAdminBox));
+      handleChat = handleChat && !(await superCheckBox(api, message, isSelf, botIsAdminBox, isAdminBox, groupSettings));
       handleChat = handleChat && !(await antiBot(api, message, groupSettings, isAdminBox, botIsAdminBox, isSelf));
       handleChat = handleChat && !(await antiSpam(api, message, groupInfo, isAdminBox, groupSettings, botIsAdminBox, isSelf));
       handleChat = handleChat && !(await antiMedia(api, message, groupSettings, isAdminBox, botIsAdminBox, isSelf));
